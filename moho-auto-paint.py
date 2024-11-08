abac3c394f180b05fc95dea45bc943c1d0afa4aa
Zloi-KoteikA <morevna@localhost.localdomain>
2024-11-08 09:55:34 +0300

    add multiple color substitution.

--- moho-auto-paint.py
+++ moho-auto-paint.py
@@ -1,61 +1,3 @@
-''' Полезный мусор
-lis=[]
-while True:
-	p=input("Введи заменяемые цвета или Enter для продолжения: ")
-	if p == '':
-		break
-	else:
-		lis.append(p)
-lis2=[]
-while True:
-	p=input("Введи цвета на замену или Enter для продолжения: ")
-	if p == '':
-		break
-	else:
-		lis2.append(p)
-
-for el in lis:
-	x=lis.index(el)
-	lis[x]=(hex_to_rgb(el))
-for el in lis:
-	x=lis.index(el)
-	p=str(el)
-	p=p.replace(',', '')
-	p=p.replace('(', '')
-	p=p.replace(')', '')
-	lis[x]=p
-titl=[]
-
-for el in lis:
-	x=lis.index(el)
-	p=str(el)
-	titl.append(p.split())
-	for i in titl:
-		e=titl.index(i)
-		titl[e]=(i)/255
-		titl[e]=(round(i, 6))
-		lis[x]=titl[e]
-for el in lis:	
-	print(el)
-
-for el in lis2:
-	x=lis2.index(el)
-	lis2[x]=(hex_to_rgb(el))
-for el in lis2:	
-	p=str(el)
-	p=p.replace(',', '')
-	p=p.replace('(', '')
-	p=p.replace(')', '')
-	lis2[x]=p
-'''
-
-
-''' Quick input
-#wl="'/home/morevna/test/Untitled.anme'"
-p="#9DA369"
-p2="#1A5727"
-#'''
-
 import os 
 
 from PIL import ImageColor
@@ -64,9 +6,6 @@ def hex_to_rgb(hex_color):
 #^^^ Convert HEX to RGB
 
 
-p=input("The color is replaceable: ")
-p2=input("The color is replaced: ")
-
 file=[]
 while True:
 	x=input("Move the files (press ENTER to continue): ")
@@ -74,7 +13,24 @@ while True:
 		break
 	else:
 		file.append(x)
-
+num1=[]
+while True:
+	x=input("The color is replaceable (press ENTER to continue): ")
+	if x == '':
+		break
+	else:
+		num1.append(x)
+num2=[]
+while True:
+	x=input("The color is replaced (press ENTER to continue): ")
+	if x == '':
+		break
+	else:
+		num2.append(x)		
+#^^^ Write data					
+		
+		
+		
 for i in file:
 	x=file.index(i)
 	i=str(i)
@@ -89,57 +45,66 @@ for i in file:
 
 
 
+for i in num1:
+	x=num1.index(i)
+	i=i.replace('#', '')
+	i=(f'#{i}')
+	i=hex_to_rgb(i)
+	i=str(i)
+	i=i.replace(',', '')
+	i=i.replace('(', '')
+	i=i.replace(')', '')
+	a,b,c=i.split()
+	a=int(a)
+	a=a/255
+	a=round(a,6)
+	if a == 1.0:
+		a=1
+	b=int(b)
+	b=b/255
+	b=round(b,6)
+	if b == 1.0:
+		b=1
+	c=int(c)
+	c=c/255
+	c=round(c,6)
+	if c == 1.0:
+		c=1
+	i= f"{a} {b} {c}"
+	num1[x]=i
+for i in num2:
+	x=num2.index(i)
+	i=i.replace('#', '')
+	i=(f'#{i}')
+	i=hex_to_rgb(i)
+	i=str(i)
+	i=i.replace(',', '')
+	i=i.replace('(', '')
+	i=i.replace(')', '')
+	a,b,c=i.split()
+	a=int(a)
+	a=a/255
+	a=round(a,6)
+	if a == 1.0:
+		a=1
+	b=int(b)
+	b=b/255
+	b=round(b,6)
+	if b == 1.0:
+		b=1
+	c=int(c)
+	c=c/255
+	c=round(c,6)
+	if c == 1.0:
+		c=1
+	i= f"{a} {b} {c}"
+	num2[x]=i	
+#^^^ Color replace
 
-p=hex_to_rgb(p)
-p=str(p)
-p=p.replace(',', '')
-p=p.replace('(', '')
-p=p.replace(')', '')
-a,b,c=p.split()
-a=int(a)
-a=a/255
-a=round(a,6)
-if a == 1.0:
-	a=1
-b=int(b)
-b=b/255
-b=round(b,6)
-if b == 1.0:
-	b=1
-c=int(c)
-c=c/255
-c=round(c,6)
-if c == 1.0:
-	c=1
-p= f"{a} {b} {c}"
-
-
-
-p2=hex_to_rgb(p2)
-p2=str(p2)
-p2=p2.replace(',', '')
-p2=p2.replace('(', '')
-p2=p2.replace(')', '')
-a2,b2,c2=p2.split()
-a2=int(a2)
-a2=a2/255
-a2=round(a2,6)
-if a2 == 1.0:
-	a2=1
-b2=int(b2)
-b2=b2/255
-b2=round(b2,6)
-if b2 == 1.0:
-	b2=1
-c2=int(c2)
-c2=c2/255
-c2=round(c2,6)
-if c2 == 1.0:
-	c2=1
-p2= f"{a2} {b2} {c2}"
 
 
 for i in file:
-	os.system(f'sed -i "s/{p}/{p2}/g" {i}')
+	for p in num1:
+		x=num1.index(p)
+		os.system(f'sed -i "s/{num1[x]}/{num2[x]}/g" {i}')
 #^^^ file rewrite
-
